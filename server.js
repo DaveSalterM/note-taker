520200aacd2cff2244ec9ce163314a31f07961df
DaveSalterM <davesalter1988@gmail.com>
2024-03-22 23:08:07 -0700

    added comments

--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const PORT = process.env.port || 3000;
 
 const app = express();
 
+//Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
 
+// Get route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//Get route for notes
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
